21841298bab632a68f4f304c20e60a0735cb2ae8
PT <PT@PT-PC>
2017-05-23 12:44:23 +0200

    - create project, add moduls

---
+++ RequestRegistry/RequestRegistry-ServiceImpl/src/main/java/hu/iit/uni/miskolc/RequestRegistry/ServiceImpl/App.java
@@ -0,0 +1,13 @@
+package hu.iit.uni.miskolc.RequestRegistry.ServiceImpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
