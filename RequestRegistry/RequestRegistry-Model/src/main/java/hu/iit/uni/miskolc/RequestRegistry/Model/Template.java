0eb8e4f563dabf1058692dd0f5c0ac9af3326e2c
PT <PT@PT-PC>
2017-05-23 14:31:58 +0200

    - create Template model

---
+++ RequestRegistry/RequestRegistry-Model/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Model/Template.java
@@ -0,0 +1,26 @@
+package hu.iit.uni.miskolc.RequestRegistry.Model;
+
+public class Template {
+
+	private String name;
+	private String description;
+	
+	public Template() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
