850d03f8e46139e70c47d5f849a1cf90829867e5
PT <PT@PT-PC>
2017-05-24 17:21:44 +0200

    - delete Admin, Student, Teacher from Model package, edit RequestDao, delete InvalidStudentException from Persist.exception package

--- RequestRegistry/RequestRegistry-Persist/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Persist/dao/RequestDao.java
+++ RequestRegistry/RequestRegistry-Persist/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Persist/dao/RequestDao.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 import hu.iit.uni.miskolc.RequestRegistry.Model.Request;
 import hu.iit.uni.miskolc.RequestRegistry.Model.RequestStatus;
-import hu.iit.uni.miskolc.RequestRegistry.Model.Student;
 import hu.iit.uni.miskolc.RequestRegistry.Model.User;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidRequestException;
-import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidStudentException;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidUserException;
 
 public interface RequestDao {
 
 	void submitRequest(Request request) throws InvalidRequestException;
 	
-	List<Request> listRequestByStudent(Student student) throws InvalidRequestException, InvalidStudentException;
+	List<Request> listRequestByUser(User user) throws InvalidRequestException, InvalidUserException;
 	
 	List<Request> listRequestByComment(String comment) throws InvalidRequestException;
 	
