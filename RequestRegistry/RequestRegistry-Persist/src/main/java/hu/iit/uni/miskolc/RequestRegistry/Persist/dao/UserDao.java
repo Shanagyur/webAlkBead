c9e25482f3d8b40f87c8d3a0bc1582aa332e03e5
PT <PT@PT-PC>
2017-05-25 18:13:24 +0200

    - edit UserDao, UserDaoImpl

--- RequestRegistry/RequestRegistry-Persist/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Persist/dao/UserDao.java
+++ RequestRegistry/RequestRegistry-Persist/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Persist/dao/UserDao.java
@@ -1,9 +1,12 @@
 package hu.iit.uni.miskolc.RequestRegistry.Persist.dao;
 
 import hu.iit.uni.miskolc.RequestRegistry.Model.Details;
+import hu.iit.uni.miskolc.RequestRegistry.Model.User;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidUserException;
 
 public interface UserDao {
 
+	User getUserById(String username) throws InvalidUserException;
+	
 	void modifyUserDetails(String username, Details newDetails) throws InvalidUserException;
 }
--- RequestRegistry/RequestRegistry-PersistImpl/src/main/java/hu/iit/uni/miskolc/RequestRegistry/PersistImpl/daoImpl/UserDaoImpl.java
+++ RequestRegistry/RequestRegistry-PersistImpl/src/main/java/hu/iit/uni/miskolc/RequestRegistry/PersistImpl/daoImpl/UserDaoImpl.java
@@ -2,14 +2,17 @@ package hu.iit.uni.miskolc.RequestRegistry.PersistImpl.daoImpl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 import hu.iit.uni.miskolc.RequestRegistry.Model.Details;
+import hu.iit.uni.miskolc.RequestRegistry.Model.User;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.dao.UserDao;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidUserException;
 import hu.iit.uni.miskolc.RequestRegistry.PersistImpl.daoImpl.converter.DetailsEntityConverter;
+import hu.iit.uni.miskolc.RequestRegistry.PersistImpl.daoImpl.converter.UserEntityConverter;
 import hu.iit.uni.miskolc.RequestRegistry.PersistImpl.daoImpl.entity.UserEntity;
 
 @Repository
@@ -22,6 +25,21 @@ public class UserDaoImpl implements UserDao {
 	public UserDaoImpl() {
 	}
 
+	@Override
+	public User getUserById(String username) throws InvalidUserException {
+		if(entityManager.find(UserEntity.class, username) == null) {
+			throw new InvalidUserException();
+		} else {
+			String select = "SELECT u FROM User u WHERE u.username = :username";
+			TypedQuery<UserEntity> query = this.entityManager.createQuery(select, UserEntity.class);
+			query.setParameter("username", username);
+			
+			UserEntity userEntity = query.getSingleResult();
+			
+			return UserEntityConverter.convertUserEntityToModel(userEntity);
+		}
+	}
+	
 	@Override
 	public void modifyUserDetails(String username, Details newDetails) throws InvalidUserException {
 		UserEntity userEntity = this.entityManager.find(UserEntity.class, username);
