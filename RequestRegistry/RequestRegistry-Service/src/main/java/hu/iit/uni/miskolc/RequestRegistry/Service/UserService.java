d2ea4c97e13ca27c756b49f3af548ca46dc2b520
PT <PT@PT-PC>
2017-05-25 18:58:18 +0200

    - edit UserService

--- RequestRegistry/RequestRegistry-Service/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Service/UserService.java
+++ RequestRegistry/RequestRegistry-Service/src/main/java/hu/iit/uni/miskolc/RequestRegistry/Service/UserService.java
@@ -1,8 +1,11 @@
 package hu.iit.uni.miskolc.RequestRegistry.Service;
 
+import hu.iit.uni.miskolc.RequestRegistry.Model.User;
 import hu.iit.uni.miskolc.RequestRegistry.Persist.exception.InvalidUserException;
 
 public interface UserService {
 
+	User getUserById(String username) throws InvalidUserException;
+	
 	void modifyUserDetails(String username, String firstname, String lastname, String email, int zip, String city, String street, String streetNumber) throws InvalidUserException;
 }
