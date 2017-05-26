13dda115014cec097b307daafd3740125a171308
PT <PT@192.168.2.12>
2017-05-26 13:27:23 +0200

    - create ErrorResponse

---
+++ RequestRegistry/RequestRegistry-Controller/src/main/java/hu/iit/uni/miskolc/RequestRegistry/controller/model/ErrorResponse.java
@@ -0,0 +1,26 @@
+package hu.iit.uni.miskolc.RequestRegistry.controller.model;
+
+public class ErrorResponse {
+
+	private String message;
+	private String reason;
+
+	public ErrorResponse() {
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+}
