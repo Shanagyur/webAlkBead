5e5b84d042726aa05b8e1f687ac97369644b7e49
PT <PT@192.168.2.12>
2017-05-26 12:30:20 +0200

    - create ServiceApplicationContextConfig

---
+++ RequestRegistry/RequestRegistry-ServiceImpl/src/main/java/hu/iit/uni/miskolc/RequestRegistry/ServiceImpl/configuration/ServiceApplicationContextConfig.java
@@ -0,0 +1,9 @@
+package hu.iit.uni.miskolc.RequestRegistry.ServiceImpl.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("hu.iit.uni.miskolc.RequestRegistry.ServiceImpl")
+public class ServiceApplicationContextConfig {
+}
