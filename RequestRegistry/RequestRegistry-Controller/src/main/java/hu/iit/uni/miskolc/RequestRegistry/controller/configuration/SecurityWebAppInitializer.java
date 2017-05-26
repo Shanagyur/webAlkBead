f8dc2146bef9ca2236ef0c60bd8f0f5d857e7e43
PT <PT@192.168.2.12>
2017-05-26 13:11:55 +0200

    - create SecurityWebAppInitializer

---
+++ RequestRegistry/RequestRegistry-Controller/src/main/java/hu/iit/uni/miskolc/RequestRegistry/controller/configuration/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package hu.iit.uni.miskolc.RequestRegistry.controller.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
